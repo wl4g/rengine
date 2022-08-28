2a96f9ede94f7f1f00e8ac67e270c8312d23c1a5
James Wong <jameswong1376@gmail.com>
2022-08-28 16:12:17 +0800

    Init Commit

---
+++ server/server-admin/src/main/java/com/wl4g/rengine/RengineParentApplication.java
@@ -0,0 +1,13 @@
+package com.wl4g.rengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RengineParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RengineParentApplication.class, args);
+	}
+
+}
---
+++ server/server-admin/src/test/java/com/wl4g/rengine/RengineParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wl4g.rengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RengineParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
